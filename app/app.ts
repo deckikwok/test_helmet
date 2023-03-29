987cd910c74e0a2a1ff4937d385c00dc2c07d020
Decki <>
2023-03-29 15:35:34 +0800

    updated for helmet to work

--- app/app.ts
+++ app/app.ts
@@ -1,11 +1,12 @@
 import express, {Express} from 'express';
-import compression from "compression"
+// import compression from "compression"
 import helmet from "helmet"
 
 const app: Express = express();
 
 // console.log(`whats compression ${compression}`)
-app.use(compression)
+// app.use(compression)
+app.use(helmet)
 
 export const server = app.listen(1111, () => {
     console.log(`[server]: Server is running at https://localhost:1111`);
